d8b4848915c7933342f92f04541a5d30bf3cdc3b
FlyingMrYu <506814597@qq.com>
2020-07-27 17:00:13 +0800

    Create systime.vue

---
+++ systime.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="system-content">
+    <span>{{date}}</span>
+  </div>
+</template>
+
+<script>
+import { getDayInter } from "@/assets/js/formatDate";
+export default {
+  name: "systime-content",
+  data() {
+    return {
+      date: null
+    };
+  },
+  created() {
+    let _this = this;
+    this.timer = setInterval(() => {
+      _this.date = getDayInter(new Date(), "yyyy-MM-dd hh:mm:ss"); // 修改数据date
+    }, 1000);
+  },
+  beforeDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer); // 在Vue实例销毁前，清除我们的定时器
+    }
+  }
+};
+</script>
+
